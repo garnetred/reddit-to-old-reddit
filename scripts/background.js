cc57309a6c1a3c2f8687436a8dd37bdc47611a2a
December G <dgarnetsmith@gmail.com>
2024-02-17 14:19:52 -0500

    Remove commented out code

--- scripts/background.js
+++ scripts/background.js
@@ -1,17 +1,3 @@
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   const tabUrl = tab.url ?? tab.pendingUrl;
-//   if (
-//     changeInfo.status === "complete" &&
-//     tabUrl &&
-//     tabUrl.includes("reddit.com")
-//   ) {
-//     chrome.scripting.insertCSS({
-//       target: { tabId: tabId },
-//       files: ["css/global.css"],
-//     });
-//   }
-// });
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   const tabUrl = tab.url ?? tab.pendingUrl;
   if (
