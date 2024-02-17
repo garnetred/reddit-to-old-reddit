eca407c309b810d41c9a1a9a21cb14a16902cc2f
December G <dgarnetsmith@gmail.com>
2024-02-17 14:07:52 -0500

    Add new images, delete old, implement basic redirect

--- scripts/background.js
+++ scripts/background.js
@@ -1,13 +1,26 @@
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   const tabUrl = tab.url ?? tab.pendingUrl;
+//   if (
+//     changeInfo.status === "complete" &&
+//     tabUrl &&
+//     tabUrl.includes("reddit.com")
+//   ) {
+//     chrome.scripting.insertCSS({
+//       target: { tabId: tabId },
+//       files: ["css/global.css"],
+//     });
+//   }
+// });
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   const tabUrl = tab.url ?? tab.pendingUrl;
   if (
     changeInfo.status === "complete" &&
     tabUrl &&
-    tabUrl.includes("linkedin.com/jobs/")
+    tabUrl.includes("www.reddit.com")
   ) {
-    chrome.scripting.insertCSS({
-      target: { tabId: tabId },
-      files: ["css/global.css"],
-    });
+    const oldRedditUrl = tabUrl.replace("www", "old");
+    // const newUrl = queryParams ? `${tedditUrl}?${queryParams}` : tedditUrl;
+    chrome.tabs.update(tabId, { url: oldRedditUrl });
   }
 });
